84ba0bb0adc61c93069bbc780134fca420a39449
Nicolas Arevalo <inag.458@gmail.com>
2022-11-17 14:36:00 -0500

    Adding pause screen

--- Assets/USCO/Scripts/PlayerController.cs
+++ Assets/USCO/Scripts/PlayerController.cs
@@ -19,6 +19,15 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update(){
+        if(GameManager.Instance.isGamePaused()){
+            Debug.Log("si entro");
+            return;
+        }else{
+            Debug.Log("No entro");
+        }
+            
+
+        
         inputMovement.x = Input.GetAxis("Horizontal");
         inputMovement.y = Input.GetAxis("Vertical");
 
@@ -30,73 +39,5 @@ public class PlayerController : MonoBehaviour
         float vel = velCaminar;
 
         rb.velocity = transform.forward * vel * inputMovement.y + transform.right * vel * inputMovement.x;
-
-        /* rotationXcamara -= inputRotation.y;
-        rotationXcamara = Mathf.Clamp(rotationXcamara, -50, 50); 
-        camara.localRotation = Quaternion.Euler(rotationXcamara, 0, 0);
-
-        transform.rotation *= Quaternion.Euler( 0, inputRotation.x, 0);
-         */
     }
 }
-
-/* public class PlayerController : MonoBehaviour
-{
-    private float horizontalMove;
-    private float verticalMove;
-    public CharacterController player;
-    private Vector3 playerInput;
-    public float _gravity = 9.8f;
-    public float _speed = 1.0f;
-
-    private Vector3 directionPlayer;
-    public Camera mainCamera;
-    private Vector3 camForward;
-    private Vector3 camRight;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = GetComponent<CharacterController>();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
-
-        playerInput = new Vector3(horizontalMove, 0, verticalMove);
-        playerInput = Vector3.ClampMagnitude(playerInput, 1);
-        camDirection();
-
-        directionPlayer = playerInput.x * camRight + playerInput.z * camForward;
-        directionPlayer = directionPlayer * _speed;
-
-        
-        player.transform.LookAt(player.transform.position + directionPlayer);
-        Debug.Log(player.velocity.magnitude);
-        SetGravity();
-        player.Move(directionPlayer * Time.deltaTime);
-    }
-
-    void camDirection()
-    {
-        camForward = mainCamera.transform.forward;
-        camRight = mainCamera.transform.right;
-
-        camForward.y = 0;
-        camRight.y = 0;
-
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-
-    }
-
-    void SetGravity()
-    {
-        directionPlayer.y = -_gravity * Time.deltaTime;
-    }
-}
- */
